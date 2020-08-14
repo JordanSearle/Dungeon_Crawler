5d0d862db7e880cb924aade065511b48afb677a0
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-08-14 11:25:59 +0100

    Randering a basic square

--- Dungeon_Crawler/openGL_init.cpp
+++ Dungeon_Crawler/openGL_init.cpp
@@ -6,6 +6,77 @@
 using namespace std;
 
 GLFWwindow* window;
+unsigned int VBO, VAO, program, EBO;
+//Takes the Users Inputs
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+void initBackground() {
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+	
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindVertexArray(VAO);
+	//Sets the Shader Locations and passes it to the class
+	ShaderInfo  shaders[] = {
+			{ GL_VERTEX_SHADER, "media/triangles.vert" },
+			{ GL_FRAGMENT_SHADER, "media/triangles.frag" },
+			{ GL_NONE, NULL }
+	};
+	//Inits the display Model
+
+	//Load Program
+	program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+
+	
+
+	
+
+
+}
+void drawBackground() {
+	glUseProgram(program);
+	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	//glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
 int main()
 {
 	glfwInit();
@@ -14,10 +85,10 @@ int main()
 	glewInit();
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	//Main render loop
+	initBackground();
 	while (!glfwWindowShouldClose(window))
 	{
-		//processInput(window);
-
+		processInput(window);
 		static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
 		glClearBufferfv(GL_COLOR, 0, black);
@@ -26,7 +97,8 @@ int main()
 		glEnable(GL_DEPTH_TEST);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
+		
+		drawBackground();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
